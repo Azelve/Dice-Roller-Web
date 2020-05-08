f5195e69514e3975b8ddf4d2e5a85737387c6194
Rodrigo Camilo <43912333+Azelve@users.noreply.github.com>
2020-05-08 14:53:02 -0300

    Deleting the main code

--- src/views/Main/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-import { Container } from "./style";
-
-const App = () => {
-  return <Container>AINDA EM PROCESSO</Container>;
-};
-
-export default App;
